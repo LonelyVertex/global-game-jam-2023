1c49f68210ad4d06e50fedca459a32602dcabeb5
Jan Slifka <jan.slifka@gmail.com>
2023-02-04 15:18:57 +0100

    Add bush health

--- Assets/Scripts/SlowBushBehaviour.cs
+++ Assets/Scripts/SlowBushBehaviour.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 
 public class SlowBushBehaviour : MonoBehaviour
 {
+    [SerializeField] int health;
+
+    int _currentHealth;
+
+    void Start()
+    {
+        _currentHealth = health;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         var enemy = other.GetComponent<EnemyBehaviour>();
         if (enemy)
         {
             enemy.AddSlowModifier();
+            ReduceHealth();
         }
     }
 
@@ -17,6 +27,15 @@ public class SlowBushBehaviour : MonoBehaviour
         if (enemy)
         {
             enemy.RemoveSlowModifier();
+        }
+    }
+
+    void ReduceHealth()
+    {
+        _currentHealth--;
+        if (_currentHealth <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
