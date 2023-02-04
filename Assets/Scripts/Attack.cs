ba6beebfcce91e4c07668da6d19281e7ac5de243
Jan Slifka <jan.slifka@gmail.com>
2023-02-04 12:03:32 +0100

    Implement attack

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -1,10 +1,32 @@
+using System.Linq;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    [SerializeField] int damage;
+    [SerializeField] float radius;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
     void Start()
     {
+        Invoke(nameof(DamageEnemies), 0.1f);
         Invoke(nameof(DestroyItself), 0.5f);
+    }
+
+    void DamageEnemies()
+    {
+        var enemies = GameObject.FindObjectsOfType<EnemyBehaviour>()
+            .Where(enemy => Vector3.Distance(transform.position, enemy.transform.position) <= radius);
+
+        foreach (var enemy in enemies)
+        {
+            enemy.TakeDamage(damage);
+        }
     }
 
     void DestroyItself()
